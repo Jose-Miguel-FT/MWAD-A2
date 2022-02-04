c00163bc6ee4dd748f2209d948ea8d8cd4a815e2
Jose-Miguel-FT <miguelfinol_48@hotmail.com>
2022-02-04 21:45:08 +0000

    1 file modified

--- src/FilterCategory.js
+++ src/FilterCategory.js
@@ -3,24 +3,32 @@ import React from 'react';
 function FilterCategory(props){
   const patientsArray = JSON.parse(props.string);
 
+  const [filter, setFilter] = React.useState('');
+
+  const Karolis = patientsArray.filter(f => f.includes(filter) || filter === '').map(f => <li key={f}>{f}</li>);
+
   return (<div>
     <br />
-<br />
-<br />
-<br />
-<p>
-  Filter patient based on the category:
-</p>
-<input
-  placeholder="Category"
-  type="text"
-  value={}
-  onChange={(event) => setStartDate(event.target.value)}
-/>
-<br />
-<br />
-<button onClick={}> FILTER </button>
-</div>
+    <br />
+    <br />
+    <br />
+    <p>
+      Filter patient based on the category:
+    </p>
+    <input
+      id="filter"
+      placeholder="Category"
+      type="text"
+      value={filter}
+      onChange={(event) => setFilter(event.target.value)}
+    />
+    <ul>
+      {Karolis}
+    </ul>
+    <br />
+    <br />
+    <button onClick={}> FILTER </button>
+    </div>
   )
 }
 
